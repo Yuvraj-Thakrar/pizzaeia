027be3544bc879464816fb80cb7eaa21a727fdd3
Yuvraj-Thakrar <75614135+Yuvraj-Thakrar@users.noreply.github.com>
2021-05-09 11:55:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+menu_list=["Chicken Tandoori Pizza","Deluxe Veggie Pizza","Panner Tikka Pizza","Veg Supreme Pizza"];
+
+function add_item(){
+    var htmlData="";
+    var item=document.getElementById("add_item").value;
+    menu_list.push(item);
+    console.log(menu_list);
+    menu_list.sort();
+console.log(menu_list);
+
+htmlData="<section class='cards'>"
+for ( i =0;i<menu_list.length;i++) {
+
+htmlData=htmlData+'<div class="card">'+'<img src="images/menu.png"/>'+menu_list[i]+'</div>';
+console.log(htmlData);
+
+}
+htmlData=htmlData+"</section>"
+
+document.getElementById("pizza").innerHTML=htmlData;
+}   
